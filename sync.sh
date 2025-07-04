f7803b5575546fa4e4d2443afc9c7b84f52cdd98
Tom <tom@bellrock.ai>
2025-07-04 10:39:15 +0100

    Tweak sync script

--- sync.sh
+++ sync.sh
@@ -4,6 +4,6 @@ IFS=$'\n\t'
 
 SOURCE_DIR="./"
 
-while inotifywait --exclude '.*\.sw[px]' -r -e modify,create,delete "${SOURCE_DIR}"; do
-  rsync --exclude '*.swp' --exclude '*.swx' -azP "${SOURCE_DIR}" pi@raspberry:~/greenhouse
+while inotifywait --exclude '(.*\.sw[px]|.*\.lock|\.git/.*)' -r -e modify,create,delete "${SOURCE_DIR}"; do
+  rsync --exclude '*.swp' --exclude '*.swx' --exclude '*.lock' --exclude '.git/' -azP "${SOURCE_DIR}" pi@raspberry:~/greenhouse
 done
